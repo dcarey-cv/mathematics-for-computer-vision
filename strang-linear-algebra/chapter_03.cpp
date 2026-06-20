f9a21a5b5b3f55df19b71c265a9b2c49df3dc473
Danny <dcarey.cv@gmail.com>
2026-06-19 20:59:08 -0500

    adds the beginning of the next chapter's program

---
+++ strang-linear-algebra/chapter_03.cpp
@@ -0,0 +1,18 @@
+/* 
+Gilbert Strang's Linear Algebra
+Covers the second part of chapter 2 (linear transformations)
+and continues into chapter 3 (orthogonality, least squares, Fourier)
+
+Objectives:
+- Get more hands-on experience with the following packages:
+  - `Eigen`:  — matrix storage and all linear algebra (the transformation matrices themselves, matrix-vector multiply, etc.)
+  - `OpenGL` via `Pangolin` wrapper: window creation, the render loop, UI controls (buttons/sliders), and issuing the raw OpenGL draw calls
+*/
+
+#include <Eigen/Dense>
+#include <pangolin/pangolin.h>
+
+int main()
+{
+  return 0;
+}
